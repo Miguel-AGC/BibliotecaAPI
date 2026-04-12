9c845ba9d799d60bac5fc3714dfdf06b9849e3fd
Miguel Garcia Campuzano <miguelagcampuzano.1410@gmail.com>
2026-04-12 01:37:07 -0600

    instalacion de paquetes Nuget a usar Conexión MySQL (appsettings.json) creacion de un DbContext

---
+++ Data/BibliotecaContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPI.Data
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using BibliotecaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BibliotecaContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("ConnectionDB"),
+    new MySqlServerVersion(new Version(8, 0, 36)))
+);
+
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
 
 var app = builder.Build();
 
