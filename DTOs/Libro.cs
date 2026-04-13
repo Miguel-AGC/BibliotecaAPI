c450f5c84e17808e2105e7b4d2ca54fef7816659
Miguel Garcia Campuzano <miguelagcampuzano.1410@gmail.com>
2026-04-13 00:35:36 -0600

    Añade DTOs para gestión de libros en BibliotecaAPI
    Se agregan las clases CrearLibroDTO, LibroDTO y ActualizarLibroDTO en el espacio de nombres BibliotecaAPI.DTOs. Estos DTOs facilitan la creación, visualización y actualización de libros, incluyendo información relevante como título, ISBN, fecha de publicación, ejemplares, autor y disponibilidad.

---
+++ DTOs/Libro.cs
@@ -0,0 +1,33 @@
+﻿namespace BibliotecaAPI.DTOs
+{
+    public class CrearLibroDTO
+    {
+        public string Titulo { get; set; }
+        public string ISBN { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int Ejemplares { get; set; }
+        public int AutorId { get; set; }
+    }
+
+    public class LibroDTO
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string ISBN { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int Ejemplares { get; set; }
+
+        public bool Disponible { get; set; }
+
+        public string Autor { get; set; }
+    }
+
+    public class ActualizarLibroDTO
+    {
+        public string Titulo { get; set; }
+        public string ISBN { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int Ejemplares { get; set; }
+        public int AutorId { get; set; }
+    }
+}
