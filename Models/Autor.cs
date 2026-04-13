8ec0761ff7daf8ad21d2b741a4563ce76ccc9da8
Miguel Garcia Campuzano <miguelagcampuzano.1410@gmail.com>
2026-04-12 23:24:44 -0600

    Añade FechaNacimiento y Nacionalidad a Autor
    Se agregan las propiedades FechaNacimiento y Nacionalidad (requerida) a la clase Autor para almacenar información adicional sobre los autores. La línea de colección de libros permanece comentada.

--- Models/Autor.cs
+++ Models/Autor.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public required string Nombre { get; set; }
 
+        public DateTime FechaNacimiento { get; set; }
+        public required string Nacionalidad { get; set; }
+
         //public ICollection<Libro> Libros { get; set; }
     }
 }
