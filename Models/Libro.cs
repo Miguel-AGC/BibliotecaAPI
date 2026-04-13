34f1e8a40a5eed620f190bc7567c5e35f9e2b965
Miguel Garcia Campuzano <miguelagcampuzano.1410@gmail.com>
2026-04-13 00:08:11 -0600

    Habilita relación Autor-Libro y añade entidad Libro
    Se ha descomentado la propiedad Libros en la clase Autor para establecer la relación uno-a-muchos con libros. Además, se ha creado la clase Libro con sus propiedades principales y la relación con Autor. Se deja preparada la posible relación con Prestamo.

---
+++ Models/Libro.cs
@@ -0,0 +1,18 @@
+﻿namespace BibliotecaAPI.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        
+        // Relación
+        public int AutorId { get; set; }
+        public Autor Autor { get; set; }
+        public string ISBN { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int Ejemplares { get; set; }
+        public bool Disponible => Ejemplares > 0;
+
+        //public ICollection<Prestamo> Prestamos { get; set; }
+    }
+}
