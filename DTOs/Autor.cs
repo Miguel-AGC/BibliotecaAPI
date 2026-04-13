cdb65fb1d14297f1d38ffa98adebfb642914dbd1
Miguel Garcia Campuzano <miguelagcampuzano.1410@gmail.com>
2026-04-12 22:46:27 -0600

    Añadir DTOs AutorDto y CrearAutorDto para autores
    Se agregan las clases AutorDto y CrearAutorDto en el espacio de nombres BibliotecaAPI.DTOs para facilitar la transferencia de datos relacionados con autores entre la API y el cliente. AutorDto incluye Id y Nombre, mientras que CrearAutorDto solo contiene Nombre.

---
+++ DTOs/Autor.cs
@@ -0,0 +1,13 @@
+﻿namespace BibliotecaAPI.DTOs
+{
+    public class AutorDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+
+    public class CrearAutorDto
+    {
+        public string Nombre { get; set; }
+    }
+}
